942689627f08cc9df9a09abec4ca5d9bed3db043
Irving Gomez <gomirv.work@gmail.com>
2024-12-26 00:04:24 -0800

    length check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageHeader from '../components/HomepageHeader';
 
 export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-
   return (
     <Layout
       title='Home'
-      description='A dedicated Full Stack Engineer with extensive experience in building scalable, secure, and user-centric applications. Proficient in both frontend and backend development, cloud computing, and CI/CD pipelines. Demonstrated success in driving projects independently, mentoring junior developers, and delivering impactful results across multiple industries.'
+      description='A Full Stack Engineer with extensive experience in building scalable, secure and user-centric applications with state-of-the-art technology.'
       wrapperClassName='index-page'
     >
       <HomepageHeader />
